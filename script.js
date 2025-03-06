b8160a05048735e438e4d216df6ad969680ebba5
rashidulislamg71 <jrashidulislam0103@gmail.com>
2025-03-07 05:46:24 +0600

    lawliet

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+$(document).ready(function(){
+    $('.testimonial-slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 2,  // ✅ Always show 2 items
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1  // 📱 Mobile: Show 1 item at a time
+                }
+            }
+        ]
+    });
+});
